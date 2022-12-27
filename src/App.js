dfa10633621b14f7f122ccea57d4293bc6892965
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-12-28 03:09:15 +0600

    task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [keys, setKeys] = useState([]);
+  const handlePostData = () => {
+    fetch("https://coderbyte.com/api/challenges/json/age-counting")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const mydata = data.data.split(",");
+        console.log(mydata);
+        const result = data.data
+          .split(",")
+          .map((x, i) => {
+            if (x.trim() == "age=30") {
+              return [i - 1];
+            }
+          })
+          .filter((x) => x != undefined)
+          .map((x) => {
+            return mydata[x].split("=")[1];
+          });
+        console.log(result);
+        setKeys(result);
+      });
+  };
+  console.log(keys);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Counting</h1>
+      <p>{keys}</p>
+      <p>Array of keys length : {keys.length}</p>
+      <button onClick={handlePostData}>Get the keys</button>
     </div>
   );
 }
